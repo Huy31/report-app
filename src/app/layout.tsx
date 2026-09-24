7cc991d4f6e09db6062875e229dd8e1e122f63d7
Huy31 <huy351587@gmail.com>
2026-09-24 10:49:49 +0700

    Gỡ bỏ Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { AppStoreProvider } from '@/data/store';
 import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: 'Cổng Báo Cáo Công Việc E-GOV • Trường ĐH Công Nghệ GTVT (UTT)',
@@ -31,7 +30,6 @@ export default function RootLayout({
       <body>
         <AppStoreProvider>{children}</AppStoreProvider>
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
